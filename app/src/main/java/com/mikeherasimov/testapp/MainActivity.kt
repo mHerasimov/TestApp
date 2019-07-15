22556284f16b272f0cc63a3b2b980db6a0141dc5
oleh.herasimov <oleh.herasimov.triare@gmail.com>
2019-07-15 13:42:17 +0300

    Handle add and remove page clicks

--- app/src/main/java/com/mikeherasimov/testapp/MainActivity.kt
+++ app/src/main/java/com/mikeherasimov/testapp/MainActivity.kt
@@ -2,11 +2,39 @@ package com.mikeherasimov.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var pagesCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter = adapter
+        fabAdd.setOnClickListener {
+            pagesCount++
+            adapter.addFragment(PageFragment.newInstance(pagesCount.toString()))
+            viewPager.setCurrentItem(pagesCount, true)
+            updateRemoveVisibility()
+        }
+        fabRemove.setOnClickListener {
+            pagesCount--
+            viewPager.setCurrentItem(pagesCount, true)
+            adapter.removeLastFragment()
+            updateRemoveVisibility()
+        }
+        fabAdd.performClick()
     }
+
+    private fun updateRemoveVisibility() {
+        if (pagesCount > 1) {
+            fabRemove.show()
+        } else {
+            fabRemove.hide()
+        }
+    }
+
 }
