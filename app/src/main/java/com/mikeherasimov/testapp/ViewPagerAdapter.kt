b7789535dd5022b46e5d2e4d375e683d348aa71e
oleh.herasimov <oleh.herasimov.triare@gmail.com>
2019-07-15 11:01:24 +0300

    Create ViewPagerAdapter

---
+++ app/src/main/java/com/mikeherasimov/testapp/ViewPagerAdapter.kt
@@ -0,0 +1,39 @@
+package com.mikeherasimov.testapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+
+
+class ViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+    private val fragments = mutableListOf<Fragment>()
+
+    override fun getItem(position: Int) = fragments[position]
+
+    override fun getCount() = fragments.size
+
+    override fun getItemPosition(`object`: Any): Int {
+        val position = fragments.indexOf(`object`)
+        if (position != -1) {
+            return PagerAdapter.POSITION_UNCHANGED
+        }
+        return PagerAdapter.POSITION_NONE
+    }
+
+    fun addFragment(fragment: Fragment) {
+        fragments.add(fragment)
+        notifyDataSetChanged()
+    }
+
+    fun removeFragmentAt(pos: Int) {
+        fragments.removeAt(pos)
+        notifyDataSetChanged()
+    }
+
+    fun removeLastFragment() {
+        removeFragmentAt(fragments.size - 1)
+    }
+
+}
